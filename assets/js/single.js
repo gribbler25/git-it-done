333d8b0d7ad6d6918c1ffe03fa45e2a58f8d602d
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-06-30 10:15:08 -0500

    finished query parameter functionality

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,10 +1,27 @@
 
 
-
-
-
 var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
+
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function () {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
+
 
 var getRepoIssues = function (repo) {
     console.log(repo);
@@ -22,7 +39,7 @@ var getRepoIssues = function (repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 
@@ -72,5 +89,5 @@ var displayWarning = function (repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-
-getRepoIssues("facebook/react");
+// getRepoIssues("facebook/react");
+getRepoName();
